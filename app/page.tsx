cd4be65ce1afd339b98c860768a71905c23d8d91
syamantak <132641600+Syamantak-Git@users.noreply.github.com>
2026-02-15 11:51:14 +0530

    add EventCard in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
+import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+import events from "@/lib/constants";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <section>
-      <h1 className="text-center">The Hub For Every Dev <br/> Event You Can't Miss</h1>
-      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
-      <ExploreBtn/>
+      <h1 className="text-center">
+        The Hub For Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+      <ExploreBtn />
       <div className="mt-20 space-y-7">
-        <h3>
-          Featured Events    
-        </h3>
+        <h3>Featured Events</h3>
         <ul className="events">
-          {[1,2,3,4,5].map((event)=>(
-            <li key={event}>Event {event}</li>
+          {events.map((event) => (
+            <li key={event.title} className="list-none"><EventCard {...event}/></li>
           ))}
         </ul>
       </div>
--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -6,7 +6,12 @@ const ExploreBtn = () => {
   return (
     <button type="button" id="explore-btn" className="mt-7 mx-auto">
       <a href="#events">Explore Events</a>
-      <Image src="/icons/arrow-down.svg" width={24} height={24} alt="down arrow"/>
+      <Image
+        src="/icons/arrow-down.svg"
+        width={24}
+        height={24}
+        alt="down arrow"
+      />
     </button>
   );
 };
